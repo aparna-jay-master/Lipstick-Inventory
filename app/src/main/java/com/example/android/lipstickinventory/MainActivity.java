fad1a9abde763f6444346feaed336e824682428b
Aparna <aparnajay.jay@gmail.com>
2017-07-13 18:58:38 -0700

    Add Lipstick Inventory App

---
+++ app/src/main/java/com/example/android/lipstickinventory/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.android.lipstickinventory;
+
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
