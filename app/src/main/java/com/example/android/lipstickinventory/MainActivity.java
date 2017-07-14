38516be2bdad5288f014fe73ecb5806b89cc5f28
Aparna <aparnajay.jay@gmail.com>
2017-07-13 21:40:29 -0700

    Create activity in MainActivity

--- app/src/main/java/com/example/android/lipstickinventory/MainActivity.java
+++ app/src/main/java/com/example/android/lipstickinventory/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.android.lipstickinventory;
 
 
+import android.app.LoaderManager;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -10,15 +17,27 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.GridView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.android.lipstickinventory.data.LipstickContract;
+import com.example.android.lipstickinventory.data.LipstickContract.LipstickEntry;
+
+public class MainActivity extends AppCompatActivity
+    implements LoaderManager.LoaderCallbacks<Cursor>{
+
+    //Identifier for lipstick data loader
+    private static final int LIPSTICK_LOADER = 0;
+
+    //Adapter for GridView
+    LipstickCursorAdapter mCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Setup FAB to open EditorActivity
+        // Setup FAB to open DetailActivity
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,5 +46,112 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        //Find GridView to populat lipstick data
+        GridView lipstickGridView = (GridView) findViewById(R.id.grid);
+
+        // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
+        View emptyView = findViewById(R.id.empty_view);
+        lipstickGridView.setEmptyView(emptyView);
+
+        // Setup an Adapter to create a list item for each row of pet data in the Cursor.
+        // There is no pet data yet (until the loader finishes) so pass in null for the Cursor.
+        mCursorAdapter = new LipstickCursorAdapter(this, null);
+        lipstickGridView.setAdapter(mCursorAdapter);
+
+        // Setup the item click listener
+        lipstickGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                // Create new intent to go to details
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+
+                // Form the content URI that represents the specific lipstick that was clicked on
+                Uri currentLipstickUri = ContentUris.withAppendedId(LipstickEntry.CONTENT_URI, id);
+
+                // Set the URI on the data field of the intent
+                intent.setData(currentLipstickUri);
+
+                // Launch the {@link EditorActivity} to display the data for the current pet.
+                startActivity(intent);
+            }
+        });
+
+        // Kick off the loader
+        getLoaderManager().initLoader(LIPSTICK_LOADER, null, this);
+    }
+
+    /**
+     * Helper method to insert hardcoded pet data into the database. For debugging purposes only.
+     */
+    private void insertInventoryData() {
+        // Create a ContentValues object where column names are the keys and we input inventory
+        ContentValues values = new ContentValues();
+        values.put(LipstickEntry.COLUMN_LIPSTICK_COLOR, "Vibrant Red");
+        values.put(LipstickEntry.COLUMN_LIPSTICK_PRICE, "1500");
+        values.put(LipstickEntry.COLUMN_LIPSTICK_QUANTITY, 7);
+
+        // Insert a new row
+        Uri newUri = getContentResolver().insert(LipstickEntry.CONTENT_URI, values);
+    }
+
+    /**
+     * Helper method to delete all inventory in the database.
+     */
+    private void deleteAllInventory() {
+        int rowsDeleted = getContentResolver().delete(LipstickEntry.CONTENT_URI, null, null);
+        Log.v("MainActivity", rowsDeleted + " rows deleted from lipstick inventory");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_catalog.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_catalog, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Insert dummy data" menu option
+            case R.id.action_insert_dummy_data:
+                insertInventoryData();
+                return true;
+            // Respond to a click on the "Delete all entries" menu option
+            case R.id.action_delete_all_entries:
+                deleteAllInventory();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle bundle) {
+        //Define projection for columns we care about
+        String [] projection = {
+                LipstickEntry._ID,
+                LipstickEntry.COLUMN_LIPSTICK_COLOR,
+                LipstickEntry.COLUMN_LIPSTICK_PRICE,
+                LipstickEntry.COLUMN_LIPSTICK_PRICE};
+
+        // This loader will execute the ContentProvider's query method on a background thread
+        return new CursorLoader(this,   // Parent activity context
+                LipstickEntry.CONTENT_URI,   // Provider content URI to query
+                projection,             // Columns to include in the resulting Cursor
+                null,                   // No selection clause
+                null,                   // No selection arguments
+                null);                  // Default sort order
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mCursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mCursorAdapter.swapCursor(null);
     }
 }
